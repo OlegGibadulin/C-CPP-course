34e76e30dd76e4abfb4d33382201421cfbe5e2a2
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-03-06 15:55:08 +0300

    Add feature of searching for optimal tickets

--- pt_1/ticket.c
+++ pt_1/ticket.c
@@ -1,16 +1,16 @@
 #include "ticket.h"
 
-int create_tickets(const int tickets_num, Ticket*** tickets) {
-    if (tickets_num < 1) {
+int create_tickets(const size_t n_tickets, Ticket*** tickets) {
+    if (n_tickets < 1) {
         return 1;
     }
 
-    *tickets = malloc(sizeof(Ticket*) * tickets_num);
+    *tickets = malloc(sizeof(Ticket*) * n_tickets);
     if (!(*tickets)) {
         return 1;
     }
 
-    for (int i = 0; i < tickets_num; ++i) {
+    for (size_t i = 0; i < n_tickets; ++i) {
         Ticket* new_ticket = malloc(sizeof(Ticket));
         if (!new_ticket) {
             delete_tickets(i, *tickets);
@@ -22,11 +22,66 @@ int create_tickets(const int tickets_num, Ticket*** tickets) {
     return 0;
 }
 
-void delete_tickets(const int tickets_num, Ticket** tickets) {
-    for (int i = 0; i < tickets_num; ++i) {
+void delete_tickets(const size_t n_tickets, Ticket** tickets) {
+    for (size_t i = 0; i < n_tickets; ++i) {
         free(tickets[i]);
     }
     free(tickets);
 }
+
+Ticket* get_optimal_price_ticket(Ticket** tickets, const size_t n_tickets,
+                                const char* dpr_code, const char* arv_code) {
+    if (!tickets || n_tickets < 1) {
+        return NULL;
+    }
+
+    size_t i_min;
+    int min_price = INT_MAX;
+
+    for (size_t i = 0; i < n_tickets; ++i) {
+        if (strcmp(tickets[i]->dpr_airport.code, dpr_code) == 0 &&
+            strcmp(tickets[i]->arv_airport.code, arv_code) == 0) {
+            if (tickets[i]->price < min_price) {
+                i_min = i;
+                min_price = tickets[i]->price;
+            }
+        }
+    }
+
+    return tickets[i_min];
+}
+
+Ticket* get_optimal_duration_ticket(Ticket** tickets, const size_t n_tickets,
+                                const char* dpr_code, const char* arv_code) {
+    if (!tickets || n_tickets < 1) {
+        return NULL;
+    }
+
+    size_t i_min;
+    tm min_dur;
+    min_dur.tm_hour = 0;
+    min_dur.tm_min = 0;
+
+    for (size_t i = 0; i < n_tickets; ++i) {
+        if (strcmp(tickets[i]->dpr_airport.code, dpr_code) == 0 &&
+            strcmp(tickets[i]->arv_airport.code, arv_code) == 0) {
+            if ((tickets[i]->fl_duration.tm_hour < min_dur.tm_hour &&
+                tickets[i]->fl_duration.tm_min < min_dur.tm_min) || i == 0) {
+                i_min = i;
+                min_dur = tickets[i]->fl_duration;
+            }
+
+        }
+    }
+
+    return tickets[i_min];
+}
+
+
+
+
+
+
+
 
 
--- pt_1/ticket.h
+++ pt_1/ticket.h
@@ -3,6 +3,11 @@
 
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <string.h>
+
+#define CHUNK_SIZE 20
 
 typedef struct tm tm;
 
@@ -13,12 +18,18 @@ typedef struct Airport {
 
 typedef struct Ticket {
     int price;
-    tm flight_duration;
-    Airport departure_airport;
-    Airport arrival_airport;
+    tm fl_duration;
+    Airport dpr_airport;
+    Airport arv_airport;
 } Ticket;
 
-int create_tickets(const int tickets_num, Ticket*** tickets);
-void delete_tickets(const int tickets_num, Ticket** tickets);
+int create_tickets(const size_t n_tickets, Ticket*** tickets);
+void delete_tickets(const size_t n_tickets, Ticket** tickets);
+
+Ticket* get_optimal_price_ticket(Ticket** tickets, const size_t n_tickets,
+                                const char* dpr_code, const char* arv_code);
+
+Ticket* get_optimal_duration_ticket(Ticket** tickets, const size_t n_tickets,
+                                const char* dpr_code, const char* arv_code);
 
 #endif /* ticket_h */
